1d0d7366c088b35df6ffa46d1a8bf3161ad16a5e
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-26 04:40:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :album
+ 
+  def initialize(name, album)
+    @name = name
+    @album = album
+  end
+ 
   
 end
